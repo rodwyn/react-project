32312413756d68fb607dccdcf9d07a1cf8177eaf
Rodwyn Moreno <rodwynmoreno@gmail.com>
2017-08-16 12:07:47 -0500

    Configured router.

--- app/config/router.jsx
+++ app/config/router.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { IndexView } from '../scripts/views/IndexView';
 import { AboutView } from '../scripts/views/AboutView';
+import { Example01 } from '../scripts/views/Example01';
+import { MyCourses } from '../scripts/views/MyCourses';
 
 const Router = ReactDOM.render((
   <BrowserRouter>
     <div>
       <Route exact path="/" component={IndexView} />
       <Route path="/about" component={AboutView} />
+      <Route path="/example01" component={Example01}  />
+      <Route path="/mycourses" component={MyCourses} />
     </div>
   </BrowserRouter>
 ), document.getElementById('app'));
